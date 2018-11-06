b014fd76dad4ff5ea56ced7ef2f9c20b2741616a
David Kane <dave.kane@gmail.com>
2018-11-06 09:38:13 -0500

    remove answers

--- scratch.R
+++ scratch.R
@@ -1,10 +1,6 @@
-# Scratch file with same example commands for working with the files in data.
+# Scratch file for working with the files in data.
 
 library(tidyverse)
 library(stringr)
 library(fs)
 
-files <- dir_ls("data")
-
-x <- files %>%
-  map_dfr(read_csv, .id = "source")
