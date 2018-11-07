2a99933a21ebbe42e85c847927a012f69788d82d
David Kane <dave.kane@gmail.com>
2018-11-07 10:30:52 -0500

    listing

--- scratch.R
+++ scratch.R
@@ -4,3 +4,12 @@ library(tidyverse)
 library(stringr)
 library(fs)
 
+# 1. Create a vector with all the file names in data/.
+
+# 2. Create a vector with just the file names that have an "A" in them.
+
+# 3. Read data/ex_926_I.csv into a tibble and provide a summary.
+
+# 4. Read in all the files into one big tibble. Check out ?map_dfr . . .
+
+# 5. Find the 4 files with the largest number of observations.
